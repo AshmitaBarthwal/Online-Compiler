37c0fa1d82796938781be6083c07a323e8499d29
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-07-09 11:56:56 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int first_number, second_number, sum;
+  cin >> first_number >> second_number;
+  sum = first_number + second_number;
+  cout<< sum;     
+  return 0;
+}
